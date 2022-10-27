67114eea7d12c9783400d609a228056cb587c30f
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-27 16:35:47 +0200

    eventos disparan métodos

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -3,9 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 0;
+
+  // sumar(): number {
+  //   return this.numero++;
+  // }
+  // restar(): number {
+  //   return this.numero--;
+  // }
+  operacion(valor: number): number {
+    return (this.numero += valor);
+  }
 }

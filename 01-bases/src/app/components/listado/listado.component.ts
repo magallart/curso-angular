ae5eb4dcdaa80b180f223b72fe09ffbf056e73ac
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-28 09:41:20 +0200

    directiva ngFor

--- 01-bases/src/app/components/listado/listado.component.ts
+++ 01-bases/src/app/components/listado/listado.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent {
-
-  constructor() { }
-
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
 }

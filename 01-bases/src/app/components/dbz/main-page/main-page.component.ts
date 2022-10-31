4a26b5a5f6e70908b0022d11a4dd338a35e62a4f
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-31 13:44:20 +0100

    two-way data binding

--- 01-bases/src/app/components/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/components/dbz/main-page/main-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 12000,
+  };
+
   agregar(): void {
-    console.log('Ey');
+    console.log(this.nuevo);
   }
 }

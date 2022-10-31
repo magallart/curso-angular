67436e1437f754e097e456e84321da778c4c04ee
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-31 12:28:04 +0100

    estructura del modulo dbz

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './components/heroes/heroes.module';
 import { ContadorModule } from './components/contador/contador.module';
+import { DbzModule } from './components/dbz/dbz.module';
+import { MainPageComponent } from './components/dbz/main-page/main-page.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { ContadorModule } from './components/contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]

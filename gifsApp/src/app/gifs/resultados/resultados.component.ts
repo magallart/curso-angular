691ccd6eee3a0f9b9cfeb234d6bcd388a721e9d3
Miguel Ángel Gallart <magallart58@gmail.com>
2022-11-15 18:11:28 +0100

    peticion http con modulo httpclient

--- gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
 })
-export class ResultadosComponent implements OnInit {
+export class ResultadosComponent {
+  constructor(private gifsService: GifsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get resultados() {
+    return this.gifsService.resultados;
   }
-
 }
--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey = 'ctQGOwMUF2OnyZcdOulJjk8tZpTQakOl';
   private _historial: string[] = [];
+  public resultados: any[] = []; // TODO cambiar tipado
 
   get historial(): string[] {
     return [...this._historial];
   }
 
-  buscarGifs(query: string = ''): void {
+  constructor(private http: HttpClient) {}
+
+  buscarGifs(query: string = '') {
     query = query.trim().toLocaleLowerCase();
 
     if (query.trim().length === 0) {
@@ -21,5 +26,14 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
+
+    this.http
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`
+      )
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
 }

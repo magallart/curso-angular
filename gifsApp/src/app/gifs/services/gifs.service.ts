95cd403b9357d5d4e3f1aa38681ec6a5ae8c601b
Miguel Ángel Gallart <magallart58@gmail.com>
2022-11-17 14:14:48 +0100

    uso del localstorage para guardar historial de busquedas

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,9 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   buscarGifs(query: string = ''): Gif[] | void {
     query = query.trim().toLocaleLowerCase();
@@ -26,6 +28,8 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http

722f801badbfcb95f5d9ac26322cd933604b11ec
Miguel Ángel Gallart <magallart58@gmail.com>
2022-11-22 13:15:43 +0100

    routing module

--- paisesapp/src/app/app.module.ts
+++ paisesapp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, PaisModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, PaisModule],
   providers: [],
   bootstrap: [AppComponent],
 })

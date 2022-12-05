d4164e0617997712334184f881ffa850ae2671e9
Miguel Ángel Gallart <magallart58@gmail.com>
2022-12-05 17:01:22 +0100

    pantalla informacion de 1 pais

--- paisesapp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ paisesapp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    // this.activatedRoute.params.subscribe(({ id }) => {
+    //   this.paisService.getPaisPorCodigo(id).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.paisService.getPaisPorCodigo(id);
+        }),
+        tap((resp) => {
+          console.log(resp);
+        })
+      )
+      .subscribe((pais) => {
+        this.pais = pais;
+      });
   }
-
 }

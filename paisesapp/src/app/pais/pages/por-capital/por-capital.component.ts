4800d4452dc62fba9462d3d7fba9fbbf59d7e47f
Miguel Ángel Gallart <magallart58@gmail.com>
2022-12-05 13:53:31 +0100

    busqueda por capital

--- paisesapp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ paisesapp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  termino = '';
+  hayError = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  buscar(termino: string): Observable<any> | void {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }

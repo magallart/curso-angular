915e5ba3babd39e3bfcff76e94ede8544118b30b
Miguel Ángel Gallart <magallart58@gmail.com>
2022-12-14 18:17:35 +0100

    pagina por region

--- paisesapp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesapp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  regionActiva = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
+  activarRegion(region: string): void {
+    if (region === this.regionActiva) {
+      return;
+    }
 
-  ngOnInit(): void {
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService.buscarRegion(region).subscribe((paises) => {
+      this.paises = paises;
+    });
   }
 
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
 }
--- paisesapp/src/app/pais/services/pais.service.ts
+++ paisesapp/src/app/pais/services/pais.service.ts
@@ -25,4 +25,9 @@ export class PaisService {
     const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country>(url);
   }
+
+  buscarRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/regionalbloc/${region}`;
+    return this.http.get<Country[]>(url);
+  }
 }

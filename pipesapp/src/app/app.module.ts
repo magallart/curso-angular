4db8fa49d40d528cfd91bb09a643eea1a007b7b9
Miguel Ángel Gallart <magallart58@gmail.com>
2022-12-20 18:04:43 +0100

    uso del menu primeng

--- pipesapp/src/app/app.module.ts
+++ pipesapp/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Módulo personalizado
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PrimeNgModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })

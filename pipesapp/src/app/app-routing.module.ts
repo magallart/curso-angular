c7defbd9df9de7df4c8825fea182a6f0439fe48b
Miguel Ángel Gallart <magallart58@gmail.com>
2022-12-23 14:09:20 +0100

    añadiendo routing

--- pipesapp/src/app/app-routing.module.ts
+++ pipesapp/src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
+import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
+import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicosComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'numeros',
+    component: NumerosComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'no-comunes',
+    component: NoComunesComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'ordenar',
+    component: OrdenarComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

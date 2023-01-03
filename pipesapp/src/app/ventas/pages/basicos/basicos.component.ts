7452591a07a7fbf33f715ac23f028cfcb470e529
Miguel Ángel Gallart <magallart58@gmail.com>
2023-01-03 16:26:52 +0100

    datepipe

--- pipesapp/src/app/ventas/pages/basicos/basicos.component.ts
+++ pipesapp/src/app/ventas/pages/basicos/basicos.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent {
   nombreLower: string = 'miguel ángel';
   nombreUpper: string = 'MIGUEL ÁNGEL';
   nombreCompleto: string = 'mIGueL ánGEl';
 
+  fecha: Date = new Date();
 }

cfeb793ac8090a40514a5f74612ce4b42b854cb6
Miguel Ángel Gallart <magallart58@gmail.com>
2023-01-03 18:10:09 +0100

    decimalpipe (number)

--- pipesapp/src/app/ventas/pages/numeros/numeros.component.ts
+++ pipesapp/src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styleUrls: ['./numeros.component.css']
+  styleUrls: ['./numeros.component.css'],
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumerosComponent {
+  ventasNetas: number = 2567789.5567;
+  porcentaje: number = 0.48;
 }

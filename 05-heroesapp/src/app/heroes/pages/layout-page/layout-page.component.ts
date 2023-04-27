1b6e7733aee65b33065f24223e2ef1a1b0ea86f9
Miguel Ángel Gallart <magallart58@gmail.com>
2023-04-27 11:35:40 +0200

    diseño de heroes layout

--- 05-heroesapp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LayoutPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LayoutPageComponent {
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
 }

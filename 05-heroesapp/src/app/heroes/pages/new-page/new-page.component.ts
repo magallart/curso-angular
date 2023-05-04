34358fbf4be9e8d4ad5d7e7bbcc2cc8f0bb40cc5
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-04 18:40:22 +0200

    mostrar heroes en cards

--- 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }

dff0b3d273288f032a4f72e8f6c1a9f435529330
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-04 18:52:32 +0200

    modificacion del pipe heroImage

--- 05-heroesapp/src/app/heroes/pipes/hero-image.pipe.ts
+++ 05-heroesapp/src/app/heroes/pipes/hero-image.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
 
 @Pipe({
-  name: 'heroImage'
+  name: 'heroImage',
 })
 export class HeroImagePipe implements PipeTransform {
+  transform(hero: Hero): string {
+    if (!hero.id && !hero.alt_img) {
+      return 'assets/no-image.png';
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (hero.alt_img) return hero.alt_img;
+
+    return `assets/heroes/${hero.id}.jpg`;
   }
-
 }

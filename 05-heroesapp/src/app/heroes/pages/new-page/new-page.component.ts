079b90e435d9e3f45ba4745b9fa370c3906fe67f
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-05 14:33:18 +0200

    creacion de formulario reactivo

--- 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [],
 })
-export class NewPageComponent implements OnInit {
+export class NewPageComponent {
   constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(): void {}
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl(''),
+    publisher: new FormControl(''),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Cómics' },

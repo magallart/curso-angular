0ff0fd0f57b066049cb958a4e2b21a10e9cf271b
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-05 13:43:31 +0200

    formulario de agregar y borrar nuevo heroe

--- 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,14 +4,15 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent implements OnInit {
+  constructor(private heroesService: HeroesService) {}
 
-  constructor(private heroesService: HeroesService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Cómics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Cómics' },
+  ];
 }

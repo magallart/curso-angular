31e70a4688217622ac653763adc8322239daee36
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-05 11:54:52 +0200

    conectar el servidor con la pagina de cada heroe

--- 05-heroesapp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroPageComponent implements OnInit {
-
-  constructor() { }
+  public hero?: Hero;
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      });
   }
-
 }

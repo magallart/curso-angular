9687c1642ae1219537b74c802d60b8983d9e15f0
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-09 12:46:52 +0200

    borrado de heroe

--- 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 05-heroesapp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -78,8 +78,12 @@ export class NewPageComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      console.log({ result });
+      if (!result) return;
+      this.heroesService
+        .deleteHeroById(this.currentHero.id)
+        .subscribe((wasDeleted) => {
+          if (wasDeleted) this.router.navigate(['/heroes']);
+        });
     });
   }
 

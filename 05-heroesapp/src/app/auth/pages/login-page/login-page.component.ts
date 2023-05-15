a300757c5f471c6a3a40043f620e43629f62cf6b
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-15 16:54:11 +0200

    metodo login

--- 05-heroesapp/src/app/auth/pages/login-page/login-page.component.ts
+++ 05-heroesapp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onLogin(): void {
+    this.authService.login('fernando@gmail.com', '123456').subscribe((user) => {
+      this.router.navigate(['/']);
+    });
   }
-
 }

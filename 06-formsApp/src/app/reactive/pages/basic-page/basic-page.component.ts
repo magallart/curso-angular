7f6c1d44d28283ff3db1c60f6e9f958d6580dba4
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-16 19:56:04 +0200

    valores iniciales del formulario y reset despues de enviar

--- 06-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+};
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
@@ -14,8 +19,13 @@ export class BasicPageComponent {
 
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
   onSave(): void {
     if (this.myForm.invalid) return;
     console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }

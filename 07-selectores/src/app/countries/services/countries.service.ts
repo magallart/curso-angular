6d9e23d142713dcfef6b8db2b6521df4f1471608
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-24 18:53:30 +0200

    metodo getCountriesByRegion en countriesservice

--- 07-selectores/src/app/countries/services/countries.service.ts
+++ 07-selectores/src/app/countries/services/countries.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import { HttpClient } from '@angular/common/http';
+import {
+  Country,
+  Region,
+  SmallCountry,
+} from '../interfaces/country.interfaces';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [
     //el guión es para indicar que es una propiedad privada y que nadie la modifique
     Region.Africa,
@@ -14,9 +23,25 @@ export class CountriesService {
     Region.Oceania,
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get regions(): Region[] {
     return [...this._regions]; // los ... mandan una copia del array y no el original para que no se pueda modificar el original
+  }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) =>
+        countries.map((country) => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+    );
   }
 }

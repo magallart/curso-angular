4fc8a8743284b9c79fc40430829d584d0f3d72f4
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-24 18:56:40 +0200

    resetear valor de pais al cambiar de region

--- 07-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 07-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-page',
@@ -34,13 +34,13 @@ export class SelectorPageComponent implements OnInit {
     this.myForm
       .get('region')!
       .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
       )
       .subscribe((countries) => {
         this.countriesByRegion = countries;
-        console.log({ countries });
       });
   }
 }

82636b792e94c5d6ff071e2458c79efa47dcd3a3
Miguel Ángel Gallart <magallart58@gmail.com>
2023-05-24 19:17:17 +0200

    aparecen las fronteras cuando seleccionas un pais

--- 07-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 07-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap, tap } from 'rxjs/operators';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,14 +11,17 @@ import { switchMap, tap } from 'rxjs/operators';
 })
 export class SelectorPageComponent implements OnInit {
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
+
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   });
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
 
   constructor(
@@ -35,12 +38,30 @@ export class SelectorPageComponent implements OnInit {
       .get('region')!
       .valueChanges.pipe(
         tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => (this.borders = [])),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
       )
       .subscribe((countries) => {
         this.countriesByRegion = countries;
+      });
+  }
+
+  onCountryChanged(): void {
+    this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value: string) => value.length > 0),
+        switchMap((alphacode) =>
+          this.countriesService.getCountryByAlphaCode(alphacode)
+        )
+      )
+      .subscribe((country) => {
+        // this.countriesByRegion = countries;
+        // console.log({ borders: country.borders });
+        this.borders = country.borders;
       });
   }
 }
